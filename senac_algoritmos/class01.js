8e626176eb901cfeb39523c7d9d839c0c39f62d2
eliasalbuquerque <78819295+eliasalbuquerque@users.noreply.github.com>
2021-04-21 22:53:04 -0300

    Create class01.js

---
+++ senac_algoritmos/class01.js
@@ -0,0 +1,15 @@
+//  alert('hello word by senac')
+//  o comando acima esta casusando os seguintes erros:
+//      . Object.<anonymous>
+//      . Module._compile
+//      . Object.Module._extensions..js
+//      . Module.load
+//      . Function.Module._load
+//      . Function.executeUserEntryPoint
+//      . internal/main/run_main_module.js
+
+//  esse não apresenta erros
+console.log('hello word by senac')
+
+var nome = prompt('Digite seu nome: ')
+console.log('Nome digitado: ' + nome)
