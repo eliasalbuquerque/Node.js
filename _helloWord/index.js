1256bf8ccf812a63e1ccfa23d6471de743d5a668
eliasalbuquerque <78819295+eliasalbuquerque@users.noreply.github.com>
2021-04-21 21:46:34 -0300

    teste 2

--- _helloWord/index.js
+++ _helloWord/index.js
@@ -1,2 +1,3 @@
-// teste -> repositorio local (git); vscode; repositorio nuvem (github)
+// teste 1 - repositorio local (git); vscode; repositorio nuvem (github); ok
+// teste 2 - rodar JS no Node.js atravez do terminal do vscode; 
 console.log('hello word!');
